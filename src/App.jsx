f464c9d53c264771cb51aecc38e4a8731ae2eb41
aaron_tetracube <aaronpsam@gmail.com>
2024-12-05 23:35:46 +0530

    added color buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-      <p>Hai</p>
-    </>
-  )
+    <div>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-sm bg-white px-3 py-2 rounded-3xl">
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
